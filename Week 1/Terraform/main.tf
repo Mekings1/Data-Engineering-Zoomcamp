19a102d238a7edfaf641c54bc0425ef140cafa75
Mekings1 <menekefrancisco@gmail.com>
2026-01-23 15:30:51 +0100

    Terraform homework completed

---
+++ Week 1/Terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "terraform-demo-bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true
+
+}
+
+resource "aws_iam_role" "demo-role" {
+  name = "zoomcamp-demo-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
